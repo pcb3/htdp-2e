9f2150d3e7a350a4b0a5ecb60a5a09851f33c778
pcb3 <petercraigblaas@gmail.com>
2019-03-13 13:04:57 +1300

    Add tests, template and definition

--- itunes.rkt
+++ itunes.rkt
@@ -3,6 +3,7 @@
 
 (require 2htdp/batch-io)
 (require 2htdp/itunes)
+(require test-engine/racket-tests)
 
 ; file-name of .xml library
 (define f-tunes "lib.xml")
@@ -83,7 +84,7 @@
   '("Track Type" "Remote")
   '("Apple Music" #true)
   '("Playlist Only" #true)) 42)
-              "Kids Return")
+              (list "Name" "Kids Return"))
 
 (define (fn-find-association key las default)
   (cond
@@ -99,5 +100,58 @@
               (first las)
               (find-association key (rest las) default))]])
 
+; LLists -> Number
+; consumes an LLists ll and outputs the total play time
+
+(check-expect (total-time/list (list '())) 0)
+
+(check-expect (total-time/list
+                               (list (list
+  '("Track ID" 22751)
+  '("Name" "184: MDN Web Docs")
+  '("Album" "The Web Platform Podcast")
+  '("Genre" "Balado")
+  '("Size" 79707353)
+  '("Total Time" 3301000)
+  (list "Date Added" (create-date 2019 3 2 8 11 58))
+  (list "Release Date" (create-date 2019 2 26 7 42 15))
+  '("Persistent ID" "8A4A55F09E3FF7D2")
+  '("Clean" #true)
+  '("Track Type" "URL")
+  '("Podcast" #true)
+  '("Unplayed" #true)
+  '("Location"
+    "http://traffic.libsyn.com/thewebplatform/WPP_184_MDN_Docs_mixdown.mp3?dest-id=209800"))
+ (list
+  '("Track ID" 22753)
+  '("Name" "185: Houdini")
+  '("Album" "The Web Platform Podcast")
+  '("Genre" "Balado")
+  '("Size" 79947095)
+  '("Total Time" 3311000)
+  (list "Date Added" (create-date 2019 3 2 8 11 58))
+  (list "Release Date" (create-date 2019 2 28 18 2 35))
+  '("Persistent ID" "2F6EB0D6CD085154")
+  '("Clean" #true)
+  '("Track Type" "URL")
+  '("Podcast" #true)
+  '("Unplayed" #true)
+  '("Location"
+    "http://traffic.libsyn.com/thewebplatform/WPP_185_Houdini_mixdown.mp3?dest-id=209800")))) 6612000)
+
+(define (fn-total-time/list)
+  (cond
+    [(empty? ll) ...]
+    [else (... (... (find-association "Total-time" (first ll) 0))
+             (fn-total-time/list (rest ll)))]))
+
+(define (total-time/list ll)
+  (cond
+    [(empty? ll) 0]
+    [else (+ (second (find-association "Total-time" (first ll) 0)
+                     (total-time/list (rest ll))))]))
+
+
+(test)
 
 
