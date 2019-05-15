be3c2d9f642092b16bbc9b51305e6d4c06c2792a
pcb3 <petercraigblaas@gmail.com>
2019-05-15 23:58:36 +1200

    Prototype of make-table

--- intermezzo2.rkt
+++ intermezzo2.rkt
@@ -21,6 +21,13 @@
 (define (make-cell n)
   `(td ,(number->string n)))
 
+; List-of-numbers List-of-numbers -> ... nested list ...
+; creates an HTML table from two lists of numbers 
+(define (make-table row1 row2)
+  `(table ((border "1"))
+          (tr ,@(make-row row1))
+          (tr ,@(make-row row2))))
+
 (check-expect `(0 ,@'(1 2 3) 4) (list 0 1 2 3 4))
 
 (check-expect `(("alan" ,(* 2 500))
@@ -48,6 +55,48 @@
                           (list 'tr (list (list 'width "200"))
                                 (list 'td "99")
                                 (list 'td "65"))))))
-              
 
+;; ex 234
+
+(define one-list
+  '("DJ Boring: Winona"
+    "Melo Ball: One"
+    "Justin Bieber: What do you mean"))
+
+; Los -> Los
+; consumes a list of strings los, reverses, ranks and re-reverses them
+(define (ranking los)
+  (reverse (add-ranks (reverse los))))
+
+; Los -> Los
+; consumes a list of strings los, and creates a list of ranked lists
+(define (add-ranks los)
+  (cond
+    [(empty? los) '()]
+    [else (cons (list (length los) (first los))
+                (add-ranks (rest los)))]))
+
+; String String -> ... deeply nested list ...
+; produces a web page with given author and title
+(define (my-first-web-page author title)
+  `(html
+     (head
+       (title ,title)
+       (meta ((http-equiv "content-type")
+              (content "text-html"))))
+     (body
+       (h1 ,title)
+       (p "I, " ,author ", made this page.")
+       (table ((border "1"))
+                        (tr ((width "800"))
+                            ,@(make-row '( 1  2)))
+                        (tr ((width "200"))
+                            ,@(make-row '(99 65)))))))
+              
+(show-in-browser
+   `(table ((border "1"))
+           (tr ((width "200"))
+               (cons (td ,(number->string (first (first (ranking one-list)))))
+               (td ,@(first (ranking one-list)))))))
+ 
               
