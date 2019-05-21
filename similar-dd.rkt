7677f0f452ab7d00eef7564efd4d6506eda4b20c
Peter-Craig <petercraigblaas@gmail.com>
2019-05-21 12:03:31 +1200

    Design occurs and s-to-end

--- similar-dd.rkt
+++ similar-dd.rkt
@@ -67,6 +67,70 @@
 ; - (cons TB '())
 ; - (cons TB List-of-[TB ITEM])
 
+; Ex 242. Here is one more parametric data definition:
+
+; A [Maybe X] is one of: 
+; – #false 
+; – X
+
+; Interpret these data definitions:
+
+; A [Maybe String] is either #false or a string
+
+; A [Maybe [List-of String]] is either #false or a list of strings
+
+; A [List-of [Maybe String]] is a list of Maybe, each structure
+; containing either #false or a string
+
+; What does the following function signature mean:
+
+; Accepts a string and a list of strings, outputs a Maybe,
+; containing #false or a list of strings
+
+; String [List-of String] -> [Maybe [List-of String]]
+; returns the remainder of los starting with s 
+; #false otherwise
+
+(check-expect (occurs "a" (list "b" "a" "d" "e"))
+              (list "d" "e"))
+
+(check-expect (occurs "a" (list "b" "c" "d")) #f)
+
+(define (fn-occurs s los)
+  (...
+   [(member? s los)
+    (... s los)]
+   [... ...]))
+
+(define (occurs s los)
+  (cond
+    [(member? s los)
+     (s-to-end s los)]
+    [else #f]))
+
+; String LoS -> LoS
+; consumes a string s and a list of strings los,
+; and ouptus a list of strings after the first occurence of s in los
+
+(check-expect (s-to-end "a" '("a" "b" "c")) '("b" "c"))
+
+(check-expect (s-to-end "a" '("a")) '())
+
+(define (fn-s-to-end s los)
+  (...
+   [(empty? (rest los)) (first los)]
+   [else (if (string=? s (first los))
+             (cons (first (rest los))
+                   (fn-s-to-end s (rest los)))
+             (fn-s-to-end s (rest los)))]))
+
+(define (s-to-end s los)
+  (cond
+    [(empty? (rest los)) (cons (first los) '())]
+    [else (if (string=? s (first los))
+              (cons (first (rest los))
+                    (s-to-end s (rest los)))
+              (s-to-end s (rest los)))]))
 
 
 
