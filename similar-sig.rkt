6aff28c7d90f04c1e06a4822617092d20d89115e
pcb3 <petercraigblaas@gmail.com>
2019-05-27 13:18:11 +1200

    Exercise 254

---
+++ similar-sig.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname similar-sig) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; 15.2 Similarities in Signatures
+
+;; Exercise 254. Formulate signatures for the following functions:
+
+; [List-of Numbers] [Number Number -> Boolean] -> [List-of Numbers] 
+
+; [List-of Strings] [String String -> Boolean] -> [List-of Strings]
+
+; [x Y] [List-of X] [X X -> Y] -> [List-of X]
+
+; [List-of IR] [IR IR -> Boolean] -> [List-of IR]
