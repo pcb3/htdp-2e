c0fa011d8cf513a8b69abf19bcbd0c4bab90ce71
pcb3 <petercraigblaas@gmail.com>
2019-08-25 20:20:01 +1200

    Design sum-from-fold and product-from-fold

--- append-from-fold.rkt
+++ append-from-fold.rkt
@@ -18,3 +18,30 @@
 
 (define (append-from-fold l1 l2)
   (foldr cons l2 l1))
+
+(define (append-from-foldl l1 l2)
+  (foldl cons l2 l1))
+
+; List-of-Numbers -> Number
+; consumes a list of numbers and produces the sum
+
+(check-expect (sum-from-fold '(0)) 0)
+
+(check-expect (sum-from-fold '(1)) 1)
+
+(check-expect (sum-from-fold '(1 2 3)) 6)
+
+(define (fn-sum-from-fold l)
+  (foldr + (... l) (... l)))
+
+(define (sum-from-fold l)
+  (foldr + (first l) (rest l)))
+
+; List-of-Numbers
+; consumes a list of numbers and produces the product
+(check-expect (product-from-fold '(0)) 0)
+(check-expect (product-from-fold '(1)) 1)
+(check-expect (product-from-fold '(1 2 2)) 4)
+
+(define (product-from-fold l)
+  (foldr * (first l) (rest l)))
