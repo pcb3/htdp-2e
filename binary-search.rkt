2b36e143739e5576121e7fea628d81111b60716e
pcb3 <petercraigblaas@gmail.com>
2020-04-10 19:16:10 +1200

    Refactor find-root

--- binary-search.rkt
+++ binary-search.rkt
@@ -23,11 +23,13 @@
     [(<= (- right left) ε) left]
     [else
       (local ((define mid (/ (+ left right) 2))
-              (define f@mid (f mid)))
+              (define f@mid (f mid))
+              (define f-left (f left))
+              (define f-right (f right)))
         (cond
-          [(or (<= (f left) 0 f@mid) (<= f@mid 0 (f left)))
+          [(or (<= f-left 0 f@mid) (<= f@mid 0 f-left))
            (find-root f left mid)]
-          [(or (<= f@mid 0 (f right)) (<= (f right) 0 f@mid))
+          [(or (<= f@mid 0 f-right) (<= f-right 0 f@mid))
            (find-root f mid right)]))]))
 
 (check-satisfied (round (poly (find-root poly 1 6)))
@@ -36,5 +38,47 @@
 ;;====
 ;; 447
 
-(find-root poly 3 5)
+(find-root poly -1 8)
+
+;;====
+;; 448
+
+; The assumption requires f left and f right to be on opposite
+; sides of the x-axis. If that is true, then we know that root
+; lies between those two points as it is defined as f(x) = 0.
+; And because the funciton is continous there must be some value
+; of x that outputs 0 and therfore the funciton must terminate.
+
+; termination argument:
+; at each step of the algorithm the search space is divided by
+; 1/2 and so we are closer to within epsilon of the root.
+
+;;====
+;; 449
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
