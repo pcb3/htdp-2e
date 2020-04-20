397b80ce6442d7a5611d9f70df991ad261c13970
pcb3 <petercraigblaas@gmail.com>
2020-04-20 19:28:18 +1200

    Create tests for file->list-of-lines

---
+++ glimpse-at-parsing.rkt
@@ -0,0 +1,33 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname glimpse-at-parsing) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/batch-io)
+
+; File -> [List-of Line]
+; converts a file into a list of lines 
+(define (file->list-of-lines afile)
+  (cond
+    [(empty? afile) '()]
+    [else
+     (cons (first-line afile)
+           (file->list-of-lines (remove-first-line afile)))]))
+             
+; File -> Line
+; consumes a file and produces a line at "\n" or at the end of
+; the file
+(define (first-line afile)
+  (cond
+    [(empty? afile) '()]
+    [(string=? (first afile) NEWLINE) '()]
+    [else (cons (first afile) (first-line (rest afile)))]))
+             
+; File -> Line
+; consumes a file and removes the the first line and returns
+; the rest of the file
+(define (remove-first-line afile)
+  (cond
+    [(empty? afile) '()]
+    [(string=? (first afile) NEWLINE) (rest afile)]
+    [else (remove-first-line (rest afile))]))
+             
+(define NEWLINE "\n") ; the 1String 
