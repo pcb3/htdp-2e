4eb76acd977bd20dd71336f0ab1615780fb71ee1
pcb3 <petercraigblaas@gmail.com>
2020-04-28 14:04:41 +1200

    Design integrate-rectangle

--- numeric-integration.rkt
+++ numeric-integration.rkt
@@ -20,4 +20,55 @@
 
 (define (integrate-kepler f a b)
   (* (/ 1 2) (- b a) (+ (f a) (f b))))
- 
+
+;;====
+;; 459
+
+(define R 10)
+
+; [Number -> Number] Number Number -> Number
+; consumes a function f and an interval a and b and produces
+; the area under the curve between the interval
+
+(check-within (integrate-rectangle
+               (lambda (x) 20) 12 22) 200 ε)
+(check-within (integrate-rectangle
+               (lambda (x) (* 2 x)) 0 10) 100 ε)
+(check-within (integrate-rectangle
+               (lambda (x) (* 3 (sqr x))) 0 10) 1000 ε)
+
+(define (fn-integrate-ractangle f a b)
+  (cond
+    [(zero? (- b a)) ...]
+    [else
+     (foldl + ... (map (... f a b)
+                       (build-list R ...)))]))    
+
+(define (integrate-rectangle f a b)
+  (cond
+    [(zero? (- b a)) 0]
+    [else
+     (foldl + 0
+            (map
+             (lambda (i)
+               (local
+                 ((define w (/ (- b a) R)))
+                 (* w (f (+ a (* i w) (/ w 2))))))
+             (build-list R (lambda (x) x))))]))
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
