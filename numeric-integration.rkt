519c3e8a4ef3ca2c50afc51f586f7c48ddc2aced
pcb3 <petercraigblaas@gmail.com>
2020-04-29 14:36:22 +1200

    Design integrate-dc

--- numeric-integration.rkt
+++ numeric-integration.rkt
@@ -6,7 +6,7 @@
 ;;====
 ;; 458
 
-(define ε 0.1)
+(define ε 0.01)
  
 ; [Number -> Number] Number Number -> Number
 ; computes the area under the graph of f between a and b
@@ -17,6 +17,8 @@
 ;(check-within (integrate-kepler (lambda (x) (* 3 (sqr x))) 0 10)
 ;              1000
 ;              ε)
+; the ablove test fails becasue it is an polynomial of degree
+; >= 2
 
 (define (integrate-kepler f a b)
   (* (/ 1 2) (- b a) (+ (f a) (f b))))
@@ -24,7 +26,7 @@
 ;;====
 ;; 459
 
-(define R 10)
+(define R 160)
 
 ; [Number -> Number] Number Number -> Number
 ; consumes a function f and an interval a and b and produces
@@ -55,15 +57,38 @@
                  ((define w (/ (- b a) R)))
                  (* w (f (+ a (* i w) (/ w 2))))))
              (build-list R (lambda (x) x))))]))
-
             
+;;====
+;; 460
 
+; [Number -> Number] Number Number -> Number
+; consumes a function f and an interval a b and produces
+; the area under the interval
+; generative: recursively dividing the area up into succesively
+; smaller pieces at each step and then applying Kepler's method
 
+(check-within (integrate-dc
+               (lambda (x) 20) 12 22) 200 ε)
+(check-within (integrate-dc
+               (lambda (x) (* 2 x)) 0 10) 100 ε)
+(check-within (integrate-dc
+               (lambda (x) (* 3 (sqr x))) 0 10) 1000 ε)
 
+(define threshold 0.01)
 
+(define (fn-integrate-dc f a b)
+  (cond
+    [(<= (- b a) threshold) (integrate-kepler ...)]
+    [else
+     (... (fn-integrate-dc ...)
+          (fn-integrate-dc ...))]))
 
-
-
+(define (integrate-dc f a b)
+  (cond
+    [(<= (- b a) threshold) (integrate-kepler f a b)]
+    [else
+     (+ (integrate-dc f a (+ a (/ (- b a) 2)))
+        (integrate-dc f (+ a (/ (- b a) 2)) b))]))
 
 
 
