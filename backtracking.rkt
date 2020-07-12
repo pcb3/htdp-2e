d8563b51939fd6917cf3b47e291707e975751f83
pc <petercraigblaas@gmail.com>
2020-07-12 14:21:33 +1200

    Design create-row

--- backtracking.rkt
+++ backtracking.rkt
@@ -3,6 +3,8 @@
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname backtracking) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; 29.2 Project: Backtracking
 
+(require 2htdp/image)
+
 (define QUEENS 8)
 ; A QP is a structure:
 ; (make-posn CI CI)
@@ -14,6 +16,9 @@
 (define QP1 (make-posn 1 0))
 (define QP2 (make-posn 0 1))
 (define QP3 (make-posn 8 8))
+
+;;====
+;; 479
 
 ; QP QP -> Boolean
 ; consumes two QP's and produces true if they are threatening
@@ -37,11 +42,11 @@
     ((define row-threat? (= (posn-x qp1) (posn-x qp2)))
      (define column-threat? (= (posn-y qp1) (posn-y qp2)))
      (define pos-diagonal-threat?
-             (= (+ (posn-x qp1) (posn-y qp1))
-                (+ (posn-x qp2) (posn-y qp2))))
+       (= (+ (posn-x qp1) (posn-y qp1))
+          (+ (posn-x qp2) (posn-y qp2))))
      (define neg-diagonal-threat?
-             (= (- (posn-x qp1) (posn-y qp1))
-                (- (posn-x qp2) (posn-y qp2)))))
+       (= (- (posn-x qp1) (posn-y qp1))
+          (- (posn-x qp2) (posn-y qp2)))))
     (or row-threat? column-threat? pos-diagonal-threat?
         neg-diagonal-threat?)))
 
@@ -50,10 +55,66 @@
     ((define row-threat? (= (posn-x qp1) (posn-x qp2)))
      (define column-threat? (= (posn-y qp1) (posn-y qp2)))
      (define pos-diagonal-threat?
-             (= (+ (posn-x qp1) (posn-y qp1))
-                (+ (posn-x qp2) (posn-y qp2))))
+       (= (+ (posn-x qp1) (posn-y qp1))
+          (+ (posn-x qp2) (posn-y qp2))))
      (define neg-diagonal-threat?
-             (= (- (posn-x qp1) (posn-y qp1))
-                (- (posn-x qp2) (posn-y qp2)))))
+       (= (- (posn-x qp1) (posn-y qp1))
+          (- (posn-x qp2) (posn-y qp2)))))
     (or row-threat? column-threat? pos-diagonal-threat?
         neg-diagonal-threat?)))
+
+;;====
+;; 480
+
+(define WIDTH 100)
+(define HEIGHT 100)
+
+(define MT (empty-scene WIDTH HEIGHT))
+(define QUEEN
+  (crop/align "center" "center" 50 50
+              (scale 0.1 (bitmap/file "chess-queen.png"))))
+(define (create-square n)
+  (square (/ WIDTH n) "outline" "black"))
+
+; Number Image -> Image
+; consumes a Number n and an Image img produces n squares
+; in a row
+(check-expect
+ (create-row 2 2)
+ (beside (create-square 2) (create-square 2)))
+(check-expect
+ (create-row 4 4)
+ (beside (create-square 4) (create-square 4)
+         (create-square 4) (create-square 4)))
+
+(define (fn-create-row n side)
+  (cond
+    [(= 1 n) ...]
+    [else
+     (beside
+      (create-square side) (fn-create-row (sub1 n) side))]))
+
+(define (create-row n side)
+  (cond
+    [(= 1 n) (create-square side)]
+    [else
+     (beside (create-square side)
+             (create-row (sub1 n) side))]))
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
